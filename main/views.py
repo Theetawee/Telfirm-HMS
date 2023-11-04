7c22dacc04060cee4f1ab706b0936b9c59442085
Forever Inc dev <redodevs@gmail.com>
2023-11-04 07:01:16 +0300

    save

--- main/views.py
+++ main/views.py
@@ -23,18 +23,16 @@ def intro(request):
 
 
 def search(request):
-    patient = request.GET.get('patient').upper()
-    if patient is None:
-        return redirect('home')
-    else:
-        patient_qs = Patient.objects.filter(
-            Q(medical_record_number__exact=patient) |
-            Q(first_name__icontains=patient) |
-            Q(last_name__icontains=patient)
-        )
-        context={
-            'qs':patient_qs
-        }
+    patient = request.POST.get('patient').upper()
+    
+    patient_qs = Patient.objects.filter(
+        Q(medical_record_number__exact=patient) |
+        Q(first_name__icontains=patient) |
+        Q(last_name__icontains=patient)
+    )
+    context={
+        'patients':patient_qs
+    }
     return render(request, 'main/search.html', context)
 
 
